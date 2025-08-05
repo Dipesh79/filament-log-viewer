657914590b79e309215f2bbc606326bee9f74cb8
Achyut Neupane <achyutkneupane@gmail.com>
2025-08-05 21:12:21 +0545

    refactor: arrow function changed to closure

--- src/LogTable.php
+++ src/LogTable.php
@@ -73,61 +73,65 @@ final class LogTable extends Page implements HasTable
     {
         return $table
             ->records(
-                fn (?array $filters, ?string $sortColumn, ?string $sortDirection, ?string $search): Collection => Collection::wrap(Log::getRows())
-                    ->map(function (array $log): array {
-                        $log['stack'] = json_decode($log['stack'] ?? []);
+                function (?array $filters, ?string $sortColumn, ?string $sortDirection, ?string $search, int $page, int $recordsPerPage): Collection {
+                    $records = Collection::wrap(Log::getRows())
+                        ->map(function (array $log): array {
+                            $log['stack'] = json_decode($log['stack'] ?? []);
 
-                        return $log;
-                    })
-                    ->when(
-                        ! $this->tableIsUnscoped(),
-                        fn (Collection $data): Collection => $data->where(
-                            'log_level',
-                            $this->activeTab
-                        ),
-                    )
-                    ->when(
-                        filled($filters['date']['from']),
-                        fn (Collection $data): Collection => $data->where(
-                            'date',
-                            '>=',
-                            $filters['date']['from']
+                            return $log;
+                        })
+                        ->when(
+                            !$this->tableIsUnscoped(),
+                            fn(Collection $data): Collection => $data->where(
+                                'log_level',
+                                $this->activeTab
+                            ),
                         )
-                    )
-                    ->when(
-                        filled($filters['date']['until']),
-                        fn (Collection $data): Collection => $data->where(
-                            'date',
-                            '<=',
-                            $filters['date']['until']
-                        )
-                    )
-                    ->when(
-                        filled($sortColumn),
-                        fn (Collection $data): Collection => $data->sortBy(
-                            $sortColumn,
-                            SORT_DESC,
-                            $sortDirection === 'desc',
-                        ),
-                        fn (Collection $data): Collection => $data->sortByDesc(
-                            'date'
-                        )
-                    )
-                    ->when(
-                        filled($search),
-                        fn (Collection $data): Collection => $data->filter(
-                            fn (array $log): bool => str_contains(
-                                mb_strtolower((string) $log['message']),
-                                mb_strtolower((string) $search)
+                        ->when(
+                            filled($filters['date']['from']),
+                            fn(Collection $data): Collection => $data->where(
+                                'date',
+                                '>=',
+                                $filters['date']['from']
                             )
                         )
-                    ))
+                        ->when(
+                            filled($filters['date']['until']),
+                            fn(Collection $data): Collection => $data->where(
+                                'date',
+                                '<=',
+                                $filters['date']['until']
+                            )
+                        )
+                        ->when(
+                            filled($sortColumn),
+                            fn(Collection $data): Collection => $data->sortBy(
+                                $sortColumn,
+                                SORT_DESC,
+                                $sortDirection === 'desc',
+                            ),
+                            fn(Collection $data): Collection => $data->sortByDesc(
+                                'date'
+                            )
+                        )
+                        ->when(
+                            filled($search),
+                            fn(Collection $data): Collection => $data->filter(
+                                fn(array $log): bool => str_contains(
+                                    mb_strtolower((string)$log['message']),
+                                    mb_strtolower((string)$search)
+                                )
+                            )
+                        )
+                        ->forPage($page, $recordsPerPage);
+                    return $records;
+                })
             ->columns([
                 TextColumn::make('log_level')
                     ->badge(),
                 TextColumn::make('env')
                     ->label('Environment')
-                    ->color(fn (string $state): array => match ($state) {
+                    ->color(fn(string $state): array => match ($state) {
                         'local' => Color::Blue,
                         'production' => Color::Red,
                         'staging' => Color::Orange,
@@ -167,7 +171,7 @@ final class LogTable extends Page implements HasTable
                     ->modalSubmitAction(false)
                     ->modalCancelAction(false)
                     ->modalHeading('Stack Trace')
-                    ->modalDescription(fn (array $record): string => $record['message'])
+                    ->modalDescription(fn(array $record): string => $record['message'])
                     ->slideOver(),
             ])
             ->poll(self::getPlugin()->getPollingTime())
