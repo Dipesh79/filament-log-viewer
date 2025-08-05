e8ae68f76e54210bebdd20448318d28e0c4f1500
Achyut Neupane <achyutkneupane@gmail.com>
2025-08-05 14:22:35 +0545

    refactor: Plugin variables separated in Traits

--- src/FilamentLogViewer.php
+++ src/FilamentLogViewer.php
@@ -4,28 +4,14 @@ declare(strict_types=1);
 
 namespace AchyutN\FilamentLogViewer;
 
+use AchyutN\FilamentLogViewer\Traits\PluginVariables;
 use Closure;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
-use Filament\Support\Concerns\EvaluatesClosures;
 
 final class FilamentLogViewer implements Plugin
 {
-    use EvaluatesClosures;
-
-    public bool|Closure $authorized = true;
-
-    public string|Closure $navigationGroup = 'System';
-
-    public string|Closure $navigationIcon = 'heroicon-o-document-text';
-
-    public string|Closure $navigationLabel = 'Log Viewer';
-
-    public int|Closure $navigationSort = 9999;
-
-    public string|Closure $navigationUrl = '/logs';
-
-    public string|null|Closure $pollingTime = null;
+    use PluginVariables;
 
     public static function make(): self
     {
@@ -107,40 +93,5 @@ final class FilamentLogViewer implements Plugin
         $this->pollingTime = $time;
 
         return $this;
-    }
-
-    public function isAuthorized(): bool
-    {
-        return $this->evaluate($this->authorized);
-    }
-
-    public function getNavigationGroup(): string
-    {
-        return $this->evaluate($this->navigationGroup);
-    }
-
-    public function getNavigationIcon(): string
-    {
-        return $this->evaluate($this->navigationIcon);
-    }
-
-    public function getNavigationLabel(): string
-    {
-        return $this->evaluate($this->navigationLabel);
-    }
-
-    public function getNavigationSort(): int
-    {
-        return $this->evaluate($this->navigationSort);
-    }
-
-    public function getNavigationUrl(): string
-    {
-        return $this->evaluate($this->navigationUrl);
-    }
-
-    public function getPollingTime(): ?string
-    {
-        return $this->evaluate($this->pollingTime);
     }
 }
---
+++ src/Traits/PluginVariables.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AchyutN\FilamentLogViewer\Traits;
+
+use Closure;
+use Filament\Support\Concerns\EvaluatesClosures;
+
+trait PluginVariables
+{
+    use EvaluatesClosures;
+
+    public bool|Closure $authorized = true;
+
+    public string|Closure $navigationGroup = 'System';
+
+    public string|Closure $navigationIcon = 'heroicon-o-document-text';
+
+    public string|Closure $navigationLabel = 'Log Viewer';
+
+    public int|Closure $navigationSort = 9999;
+
+    public string|Closure $navigationUrl = '/logs';
+
+    public string|null|Closure $pollingTime = null;
+
+    public function isAuthorized(): bool
+    {
+        return $this->evaluate($this->authorized);
+    }
+
+    public function getNavigationGroup(): string
+    {
+        return $this->evaluate($this->navigationGroup);
+    }
+
+    public function getNavigationIcon(): string
+    {
+        return $this->evaluate($this->navigationIcon);
+    }
+
+    public function getNavigationLabel(): string
+    {
+        return $this->evaluate($this->navigationLabel);
+    }
+
+    public function getNavigationSort(): int
+    {
+        return $this->evaluate($this->navigationSort);
+    }
+
+    public function getNavigationUrl(): string
+    {
+        return $this->evaluate($this->navigationUrl);
+    }
+
+    public function getPollingTime(): ?string
+    {
+        return $this->evaluate($this->pollingTime);
+    }
+}
